8c4fcdd10bd8897bce4c8c05b01efc8a7a2119cb
iamlucasgomes <lucas.devjs@gmail.com>
2023-06-10 01:27:53 -0300

    feat: Palindromes (Iteractivity)

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,11 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+    low_index = 0
+    high_index = len(word) - 1
+    while low_index < high_index:
+        if word[low_index] != word[high_index]:
+            return False
+        low_index += 1
+        high_index -= 1
+    return True
