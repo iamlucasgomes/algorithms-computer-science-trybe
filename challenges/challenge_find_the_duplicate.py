f8f9213a35473538a757fac611b9644775b3adb1
iamlucasgomes <lucas.devjs@gmail.com>
2023-06-10 01:16:25 -0300

    feat: Finding Repeated Numbers (Search Algorithm)

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,12 @@
+from collections import Counter
+
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if (
+        not nums
+        or not all(isinstance(num, int) for num in nums)
+        or any(num < 0 for num in nums)
+        or len(nums) == len(set(nums))
+    ):
+        return False
+    return Counter(nums).most_common(1)[0][0]
