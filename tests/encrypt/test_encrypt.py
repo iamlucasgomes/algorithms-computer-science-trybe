2f4bfdadb86956943638ad85d56c6d6cf3ad90cb
iamlucasgomes <lucas.devjs@gmail.com>
2023-06-09 23:22:18 -0300

    test: Inversion encryption (Tests)

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,16 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+from pytest import raises
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("abc", 1) == "a_cb"
+    assert encrypt_message("abc", 2) == "c_ba"
+    assert encrypt_message("abc", 3) == "cba"
+
+    with raises(TypeError) as error:
+        encrypt_message("abc", "")
+    assert str(error) == "tipo inválido para key"
+
+    with raises(TypeError) as error:
+        encrypt_message(1, 1)
+    assert str(error) == "tipo inválido para message"
