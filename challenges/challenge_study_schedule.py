ed30802fdd6a83a83b1dc2f0b91f355d57cf9b3a
iamlucasgomes <lucas.devjs@gmail.com>
2023-06-09 22:44:05 -0300

    feat: Number of students studying at the same time (Search algorithm)

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código here."""
-    raise NotImplementedError
+    if not target_time:
+        return None
+    students = 0
+    for start_period, end_period in permanence_period:
+        if not isinstance(start_period, int) or not isinstance(
+            end_period, int
+        ):
+            return None
+        if start_period <= target_time <= end_period:
+            students += 1
+    return students
